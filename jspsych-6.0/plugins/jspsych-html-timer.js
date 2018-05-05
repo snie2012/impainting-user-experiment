4ccc884316eece4ef2766f7c6867f8ca6a9fd79a
Shaoliang Nie <snie@ncsu.edu>
2018-05-05 13:52:17 -0400

    update

---
+++ jspsych-6.0/plugins/jspsych-html-timer.js
@@ -0,0 +1,35 @@
+/*
+ * Html timer
+ */
+
+jsPsych.plugins["PLUGIN-NAME"] = (function() {
+
+  var plugin = {};
+  
+  plugin.info = {
+    name: "html-timer",
+    parameters: {
+      parameter_name: {
+        type: jsPsych.plugins.parameterType.INT, // INT, IMAGE, KEYCODE, STRING, FUNCTION, FLOAT
+        default_value: undefined
+      },
+      parameter_name: {
+        type: jsPsych.plugins.parameterType.IMAGE,
+        default_value: undefined
+      }
+    }
+  }
+
+  plugin.trial = function(display_element, trial) {
+
+    // data saving
+    var trial_data = {
+      parameter_name: 'parameter value'
+    };
+
+    // end trial
+    jsPsych.finishTrial(trial_data);
+  };
+
+  return plugin;
+})();
